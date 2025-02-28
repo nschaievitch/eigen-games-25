1e94fa26be8ea385a6073b97866d6e1f1f1e8214
Nicolas Schaievitch <nicolas.schaievitch@gmail.com>
2025-02-27 20:13:00 -0700

    fetch image

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,19 +1,19 @@
 mod cryptography;
 mod images;
+use base64::{Engine as _, engine::general_purpose};
+use clap::{Args, Parser, Subcommand};
+use dotenv::dotenv;
+use eyre::Result;
+use futures::executor::block_on;
 use image::{GenericImageView, imageops::FilterType};
 use images::save_image;
 use pinata_sdk::PinByJson;
-use tfhe::boolean::backward_compatibility::public_key;
-use std::{env, fs::File, io::Read};
-use base64::{Engine as _, engine::general_purpose};
-use clap::{Args, Parser, Subcommand};
-use eyre::Result;
-use tokio;
-use std::path::PathBuf;
-use dotenv::dotenv;
 use std::collections::HashMap;
-use futures::executor::block_on;
-
+use std::path::PathBuf;
+use std::{env, fs::File, io::Read};
+use tfhe::boolean::backward_compatibility::public_key;
+use tokio;
+use serde_json::{Map, Value};
 
 
 #[derive(Parser)]
@@ -26,12 +26,15 @@ struct Cli {
 enum Commands {
     /// Generate a new set of encryption keys
     GenerateKeys,
-    
+
     /// Process and encrypt an image
     ProcessImage(ProcessImageArgs),
-    
+
     /// Decrypt an encrypted image
     DecryptImage(DecryptImageArgs),
+
+    /// Fetch image from IPFS proof of task
+    FetchImage(FetchImageArgs),
 }
 
 #[derive(Args)]
@@ -42,10 +45,10 @@ struct ProcessImageArgs {
 
     /// Path to the image
     image: PathBuf,
-    
+
     /// URL for the ExecutionService
     #[arg(long, default_value = "http://localhost:4003")]
-    execution_url: String
+    execution_url: String,
 }
 
 #[derive(Args)]
@@ -53,17 +56,32 @@ struct DecryptImageArgs {
     /// Path to the encryption keys
     #[arg(long)]
     keys: PathBuf,
-    
+
     /// The path to the encrypted image
     encrypted_image: PathBuf,
 
     /// Path to save image at
     #[arg(long)]
-    save_path: PathBuf
+    save_path: PathBuf,
+}
+
+#[derive(Args)]
+struct FetchImageArgs {
+    /// Path to the encryption keys
+    #[arg(long)]
+    keys: PathBuf,
+
+    /// IPFS Hash of the proof of task
+    ipfs_hash: String,
+
+    /// Path to save image at
+    #[arg(long)]
+    save_path: PathBuf,
 }
 
 #[tokio::main]
-async fn main() -> Result<()>{
+async fn main() -> Result<()> {
+    dotenv().ok();
     let cli = Cli::parse();
 
     match &cli.command {
@@ -76,13 +94,16 @@ async fn main() -> Result<()>{
         Commands::DecryptImage(args) => {
             decrypt_image(&args.keys, &args.encrypted_image, &args.save_path)?;
         }
+        Commands::FetchImage(args) => {
+            fetch_image(&args.keys, &args.ipfs_hash, &args.save_path).await?;
+        }
     }
 
     Ok(())
 }
 
 // Mock functions - to be implemented later
-fn generate_keys() -> Result<()>{
+fn generate_keys() -> Result<()> {
     // This function will generate encryption keys
     let keys = cryptography::Keys::new();
     keys.save("keys.bin".to_string())?;
@@ -91,9 +112,12 @@ fn generate_keys() -> Result<()>{
     Ok(())
 }
 
-async fn process_image(keys_path: &PathBuf, image: &PathBuf, execution_service: &String) -> Result<()>{
-    dotenv().ok();
-    let keys = cryptography::Keys::load(keys_path.to_string_lossy().to_string())?; 
+async fn process_image(
+    keys_path: &PathBuf,
+    image: &PathBuf,
+    execution_service: &String,
+) -> Result<()> {
+    let keys = cryptography::Keys::load(keys_path.to_string_lossy().to_string())?;
     let image_arr = images::load_image(image.to_string_lossy().to_string())?;
 
     let enc = keys.enc_array(image_arr);
@@ -113,21 +137,44 @@ async fn process_image(keys_path: &PathBuf, image: &PathBuf, execution_service: 
 
     println!("Task created");
 
-
     Ok(())
 }
 
-fn decrypt_image(keys_path: &PathBuf, encrypted_image: &PathBuf, image_path: &PathBuf) -> Result<()> {
-    let keys = cryptography::Keys::load(keys_path.to_string_lossy().to_string())?; 
-
+fn decrypt_image(
+    keys_path: &PathBuf,
+    encrypted_image: &PathBuf,
+    image_path: &PathBuf,
+) -> Result<()> {
+    let keys = cryptography::Keys::load(keys_path.to_string_lossy().to_string())?;
 
     let mut file = File::open(encrypted_image)?;
     let mut enc_b64 = Vec::<u8>::new();
     file.read_to_end(&mut enc_b64)?;
-    
+
     let enc = cryptography::decode_enc_image(&String::from_utf8(enc_b64)?)?;
     let dec = keys.dec_array(enc);
 
+    images::save_image(&dec, image_path.to_string_lossy().to_string())?;
+
+    Ok(())
+}
+
+async fn fetch_image(
+    keys_path: &PathBuf,
+    ipfs_hash: &String,
+    image_path: &PathBuf
+) -> Result<()> {
+    let keys = cryptography::Keys::load(keys_path.to_string_lossy().to_string())?;
+    
+    let data = reqwest::get(format!("https://ipfs.io/ipfs/{}", ipfs_hash)).await?;
+    let value: Value = data.json().await?;
+    let image_val = value.get("image").unwrap();
+    let image = image_val.as_str().unwrap();
+
+    let enc = cryptography::decode_enc_image(&image.to_string())?;
+    
+    let dec = keys.dec_array(enc);
+    
     images::save_image(&dec, image_path.to_string_lossy().to_string())?;
 
     Ok(())
